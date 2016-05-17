30e754e2e23687e493c155b98b9f0a23277a7d2c
Patrick Hund <pahund@team.mobile.de>
2016-05-17 19:38:39 +0200

    fixed warning about missing key
    Change-Id: I2701925ee5002627e40b1074b641bbe2746cd026

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom";
 
 class HelloMessage extends React.Component {
     render() {
+        let index = 0;
         return (
             <h1>
                 {
                     this.props.greetings.map(greeting => {
-                        return <span>{greeting} {this.props.who}!<br /></span>;
+                        return <span key={`greeting${index++}`}>{greeting} {this.props.who}!<br /></span>;
                     })
                 }
             </h1>
