c80e04640d0888c893a57200e55e3a2286d98384
Patrick Hund <pahund@team.mobile.de>
2016-05-17 20:32:35 +0200

    posts
    Change-Id: If44e1d738e5dcd168f8d8fca1afe1e4227ab5e6b

--- src/index.js
+++ src/index.js
@@ -6,13 +6,25 @@ import data from "json!../data/forum.json";
 class Forum extends React.Component {
     render() {
         console.log(this.props.topics);
-        let topicIndex = 0;
+        let topicIndex = 0,
+            postIndex = 0;
         return (
             <div>
                 {this.props.topics.map(topic => {
                     return (
                         <article key={`topic${topicIndex++}`}>
                             <h1>{topic.subject}</h1>
+                            {topic.posts.map(post => {
+                                return (
+                                    <section key={`post${postIndex++}`}>
+                                        <div>{post.message}</div>
+                                        <div>
+                                            <img src={`images/avatar-${post.poster.id}.gif`} />
+                                            <b>{post.poster.name}</b> {post.date}
+                                        </div>
+                                    </section>
+                                );
+                            })}
                         </article>
                     );
                 })}
