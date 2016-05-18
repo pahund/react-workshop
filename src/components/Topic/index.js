c18bf2683a287f0cb702accb80bcdab3a4f27343
Patrick Hund <pahund@team.mobile.de>
2016-05-18 13:53:02 +0200

    making topic stateful components
    Change-Id: I9677f3684611095c5430366176a3f6e663a704d5

--- src/components/Topic/index.js
+++ src/components/Topic/index.js
@@ -2,6 +2,14 @@ import React from "react";
 import styles from "./styles.css";
 
 class Topic extends React.Component {
+    constructor(props) {
+        super(props);
+        this.props = props;
+        this.state = {
+            expanded: false
+        };
+    }
+
     render() {
         return (
             <article className={styles.topic}>
