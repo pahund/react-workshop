d24d6f0a7c1ec5ba6b243eb0b77a8fb954d7cbe4
Patrick Hund <pahund@team.mobile.de>
2016-05-19 18:49:45 +0200

    fetching data with axios
    Change-Id: I2cdf1539c4ea383aa33f643a62c454d56cb6f159

--- src/index.js
+++ src/index.js
@@ -1,21 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./containers/App";
-import data from "json!../data/forum.json";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./reducers";
+import axios from "axios";
 
-const store = createStore(
-    rootReducer,
-    {
-        topics: data.topics
-    }
-);
+axios.get("http://localhost:8080/api/topics").then(response => {
+    const topics = response.data;
 
-ReactDOM.render(
-    <Provider store={store}>
-        <App topics={data.topics} />
-    </Provider>,
-    document.getElementById("main")
-);
+    const store = createStore(
+        rootReducer,
+        {
+            topics
+        }
+    );
+
+    ReactDOM.render(
+        <Provider store={store}>
+            <App topics={topics} />
+        </Provider>,
+        document.getElementById("main")
+    );
+});
+
+
