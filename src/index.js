767fea1071ec7719ec3bbf08a2abc8015fb14402
Patrick Hund <pahund@team.mobile.de>
2016-05-20 08:19:03 +0200

    adding redux saga to the stack
    Change-Id: Ic92fe7250e55bade51d3bce42e3b420c1351d099

--- src/index.js
+++ src/index.js
@@ -5,16 +5,20 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./reducers";
 import axios from "axios";
+import createSagaMiddleware from "redux-saga";
+
 
 axios.get("http://localhost:8080/api/topics").then(response => {
     const topics = response.data;
-
+    const sagaMiddleware = createSagaMiddleware();
     const store = createStore(
         rootReducer,
         {
             topics
-        }
+        },
+        applyMiddleware(sagaMiddleware)
     );
+    sagaMiddleware.run(postComment);
 
     ReactDOM.render(
         <Provider store={store}>
---
+++ src/sagas/postComment.js
@@ -0,0 +1,6 @@
+/**
+ * postComment.js
+ *
+ * @author <a href="mailto:pahund@team.mobile.de">Patrick Hund</a>
+ * @since 19 May 2016
+ */
