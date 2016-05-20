ed80b2530adf8932df97f73ee75f6f1f4db8929a
Patrick Hund <pahund@team.mobile.de>
2016-05-20 08:30:29 +0200

    post comment saga
    Change-Id: I23f65cf61e757111146943dbf2f1a5454a84f0a5

--- src/sagas/postComment.js
+++ src/sagas/postComment.js
@@ -4,3 +4,22 @@
  * @author <a href="mailto:pahund@team.mobile.de">Patrick Hund</a>
  * @since 19 May 2016
  */
+import axios from "axios";
+
+function *postCommentSaga() {
+    yield *takeEvery("COMMENT_POSTED", postComment);
+}
+
+function *postComment(action) {
+    yield call(postCommentToApi, action.topicId, action.message);
+}
+
+function postCommentToApi(topicId, message) {
+    axios.post(`http://localhost:8080/api/topics/${topicId}`, {
+        message,
+        userId: 2314,
+        userName: "ChubberGhouly"
+    })
+}
+
+export default postCommentSaga;
